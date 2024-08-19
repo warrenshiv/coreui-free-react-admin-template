ea71fb13569bc554698e133702d9d1c27c6140b3
warrenshiv <warrenshiv@gmail.com>
2024-08-19 07:29:22 +0300

    Initial COmmit

---
+++ src/views/theme/typography/Typography.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { CCard, CCardHeader, CCardBody } from "@coreui/react";
+import { DocsLink } from "src/components";
+
+const Typography = () => {
+  return (
+    <>
+      <h1>Warren</h1>
+    </>
+  );
+};
+
+export default Typography;
